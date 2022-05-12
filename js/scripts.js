7cd8ffe64d24cc226c6eac1281a23d788cb32e9a
Miguel Semedo <99690612+msemedo2@users.noreply.github.com>
2022-05-12 18:49:58 +0100

    pokemon list array added in js file

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,22 @@
-alert('Hello world');
+// alert('Hello world');
 
-let favoriteFood = 'Pizza';
-document.write(favoriteFood);
+// let favoriteFood = 'Pizza';
+// document.write(favoriteFood);
+
+let pokemonList = [
+  {
+    name: 'Bulbasaur',
+    height: 7,
+    types: ['grass', 'poison']
+  },
+  {
+    name: 'Charizard',
+    height: 8,
+    types: ['fire', 'flying']
+  },
+  {
+    name: 'Blastoise',
+    height: 9,
+    types: ['water', 'ground']
+  }
+];
